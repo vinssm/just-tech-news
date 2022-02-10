4c53874af4f2ab973cb4bfbc7aed03cc1e089d23
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-02-09 20:09:30 -0500

    all done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,7 +21,9 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
